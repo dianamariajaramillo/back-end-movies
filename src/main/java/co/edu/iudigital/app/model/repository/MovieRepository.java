b1919baa9cb2f7f2e88bb193ceaf1ac4fe297765
dianamariajaramillo <dimajao@hotmail.com>
2021-04-17 18:24:06 -0500

    envio del back-end completo

--- src/main/java/co/edu/iudigital/app/model/repository/MovieRepository.java
+++ src/main/java/co/edu/iudigital/app/model/repository/MovieRepository.java
@@ -3,7 +3,7 @@ package co.edu.iudigital.app.model.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import co.edu.iudigital.app.model.entity.Movie;
-//prueba
+
 public interface MovieRepository extends CrudRepository<Movie, Integer>{
 
 	
--- src/main/java/co/edu/iudigital/app/model/repository/UserRepository.java
+++ src/main/java/co/edu/iudigital/app/model/repository/UserRepository.java
@@ -6,5 +6,6 @@ import co.edu.iudigital.app.model.entity.User;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
 
+	public User findByEmailAndPassword(String email, String password);
 
 }
